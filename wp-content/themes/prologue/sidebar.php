86535c5c9fa52de2bd6d16ffe11ec123bd29d78b
Alexandre Girard <alx.girard@gmail.com>
2008-05-22 13:44:26 +0200

    correct display of login link

--- wp-content/themes/prologue/sidebar.php
+++ wp-content/themes/prologue/sidebar.php
@@ -17,9 +17,9 @@ if( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) {
 			<p>
 				<?php
 				if ( ! is_user_logged_in() )
-					$link = '<a href="' . get_option('siteurl') . '/wp-login.php?redirect_to=' . urlencode(get_option('siteurl')) . '">' . __('Log in') . '</a>';
+					echo '<a href="' . get_option('siteurl') . '/wp-login.php?redirect_to=' . urlencode(get_option('siteurl')) . '">' . __('Log in') . '</a>';
 				else
-					$link = '<a href="' . get_option('siteurl') . '/wp-login.php?action=logout">' . __('Log out') . '</a>';
+					echo '<a href="' . get_option('siteurl') . '/wp-login.php?action=logout">' . __('Log out') . '</a>';
 				?>
 			</p>
 		</li>
