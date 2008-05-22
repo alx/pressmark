6be44a79c364ef374d878182be312f15bf0c2882
Alexandre Girard <alx.girard@gmail.com>
2008-05-22 21:28:59 +0200

    add favicon link

--- wp-content/themes/prologue/header.php
+++ wp-content/themes/prologue/header.php
@@ -8,6 +8,7 @@
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+		<link rel="shortcut icon" href="favicon.ico">
 		<?php wp_head(); ?>
 	</head>
 <body>
