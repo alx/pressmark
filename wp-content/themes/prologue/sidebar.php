c43f635be882ea29d9ba16fd676093cdfe42b2df
Alexandre Girard <alx.girard@gmail.com>
2008-05-22 21:21:10 +0200

    change press-it link

--- wp-content/themes/prologue/sidebar.php
+++ wp-content/themes/prologue/sidebar.php
@@ -18,10 +18,7 @@
 		<li class="bookmarlet">
 			<p>NOT WORKING YET - Copy this link in your Firefox Bookmarks Toolbar!<br/>
 			Then when you get a link, simply press the "Press it" bookmark to post it on <?php bloginfo( 'name' ); ?>.<br/>
-			<a href="javascript:Q='';if(top.frames.length==0)Q=document.selection.createRange()
-			.text;void (btw=window.open ('<?php echo get_option('siteurl'); ?>?postcontent='+escape(Q)+
-			'&posturl='+escape (location.href)+'&posttitle='+escape(document.title),'bookmarklet'
-			,&'scrollbars=yes,width=600,height=460,left=100,top=150,status=yes'));btw.focus();">
+			<a href="javascript:Q='';if(navigator.userAgent.indexOf('Safari')>=0){Q=getSelection();}else{Q=document.selection?document.selection.createRange().text:document.getSelection();}location.href='<?php echo get_option('siteurl');?>index.php?posttext='+encodeURIComponent(Q)+'&posturl='+encodeURIComponent(location.href)+'&posttitle='+encodeURIComponent(document.title);">
 			Press it</a>
 			</p>
 		</li>
