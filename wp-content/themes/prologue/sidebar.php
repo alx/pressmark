315f7805449a1580bf9284f7f799dd4981d458e9
Alexandre Girard <alx.girard@gmail.com>
2008-05-22 15:04:52 +0200

    use tag_cloud

--- wp-content/themes/prologue/sidebar.php
+++ wp-content/themes/prologue/sidebar.php
@@ -1,17 +1,7 @@
 
 <div id="sidebar">
 	<ul>
-
-<?php 
-if( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) { 
-	$before = "<li><h2>Recent Tags</h2>\n";
-	$after = "</li>\n";
-
-	$num_to_show = 35;
-
-	echo prologue_recent_projects( $num_to_show, $before, $after );
-} // if dynamic_sidebar
-?>
+		<li><?php wp_tag_cloud(''); ?></li>
 
 		<li class="credits">
 			<p>
