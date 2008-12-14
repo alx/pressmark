6f3aa29b7de2bc5f30010ab91b23c19338d5ec55
Alexandre Girard <alx.girard@gmail.com>
2008-12-14 12:50:52 +0100

    Add ubiquity command

--- wp-content/themes/column/footer.php
+++ wp-content/themes/column/footer.php
@@ -3,6 +3,8 @@
 
             &bull; Designed from <a href="http://givemesomethingtoread.com/">Give Me Something To Read</a>.
             <br/>
+			Ubiquity Command: <b>press</b><link rel="commands" href="<?php bloginfo('template_url'); ?>/ubiquity.js" name="press" />
+			<br/>
             Send questions or comments to <a href="mailto:alx.girard@gmail.com">alx.girard@gmail.com</a>.
         </div>
 
